49a7340e621584b0f2140083c53ab849f11166fe
Noah Greer <noah.greer@gmail.com>
2019-12-08 14:46:29 -0800

    SignalControlledVoltageSource

---
+++ include/SignalBlocks/SignalControlledVoltageSource.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "MnaBlock.h"
+#include "SignalBlock.h"
+
+namespace ELCT350
+{
+  namespace Signal
+  {
+    class SignalControlledVoltageSource final : public MnaBlock, public SignalBlock
+    {
+    public:
+      #pragma region Enumerations
+	  enum MnaPorts : size_t
+	  {
+		Negative,
+		Positive,
+		SupplyCurrent_Internal
+	  };
+
+	  enum Parameters : size_t
+	  {
+		MaxVoltage,
+		Gain
+	  };
+
+	  enum InputSignalPorts : size_t
+	  {
+		Signal
+	  };
+      #pragma endregion
+
+      #pragma region Constructors and Destructors
+	  SignalControlledVoltageSource();
+      #pragma endregion
+
+      #pragma region MnaBlock
+      virtual void step(double timeStep, double dCurrentTime) override;
+      #pragma endregion
+    };
+  }
+}
